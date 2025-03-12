ab8c9259b2b9f9c6f245e1e2bad32cc49353b7da
acredsfan <65324894+acredsfan@users.noreply.github.com>
2025-03-12 11:05:29 -0400

    Separate package installation logic into a new module.
    Moved virtual environment setup and package installation logic from `vin_decoder.py` to a new file, `install_required_packages.py`. This improves code organization and ensures modularity by decoupling dependency management from the main application logic.

--- vin_decoder.py
+++ vin_decoder.py
@@ -1,35 +1,4 @@
-import subprocess
-import sys
 import os
-
-
-# Check and create virtual environment if it doesn't exist
-def setup_virtualenv(venv_name='venv'):
-    if not os.path.exists(venv_name):
-        subprocess.check_call([sys.executable, '-m', 'venv', venv_name])
-
-    if sys.platform == 'win32':
-        activate_script = os.path.join(venv_name, 'Scripts', 'python.exe')
-    else:
-        activate_script = os.path.join(venv_name, 'bin', 'python')
-
-    return activate_script
-
-
-# Automatically install missing dependencies
-def install_packages(packages, python_exec):
-    subprocess.check_call([python_exec, '-m', 'pip', 'install', '--upgrade', 'pip'])
-    for package in packages:
-        subprocess.check_call([python_exec, "-m", "pip", "install", package])
-
-
-# Setup virtual environment and get Python executable
-python_executable = setup_virtualenv()
-
-# List of required packages
-required_packages = ["flask", "requests", "pandas", "werkzeug", "flask_limiter", "openpyxl", "pyngrok", "pandas", "python-dotenv"]
-install_packages(required_packages, python_executable)
-
 from flask import Flask, request, render_template, send_file, redirect, url_for, session
 import requests
 import pandas as pd
